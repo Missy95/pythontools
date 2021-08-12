9a1aeff9af6c994fb483eddd09746ad1b3c389b3
missy95 <missy95@163.com>
2021-08-12 14:24:54 +0800

    添加检查表名函数

--- user_group_tool.py
+++ user_group_tool.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import re
 
 dbconfig = {'user': 'userprofile',
             'pwd': 'userprofile',
@@ -12,19 +13,42 @@ def read_excel():
     return read
 
 
-if __name__ == '__main__':
-    print('请输入要插入数据的表名：')
-    table_name = str(input())
+def retry_in():
+    print('输入的表名有误！请重新输入：')
+    newval = str(input())
+    return check_table(newval)
+
+
+def check_table(tname):
+    if tname.startswith('up_user_group_0'):
+        lastwo = tname[15:]
+        if lastwo != '' and len(lastwo) == 2:
+            re_tname = re.compile(r'^[0-2][1-9]|[1-3]0')
+            if re.match(re_tname, lastwo):
+                into_db(tname)
+            else:
+                retry_in()
+        else:
+            retry_in()
+    else:
+        retry_in()
+
+
+def into_db(name):
     connect_str = 'mysql+pymysql://%s:%s@%s/%s' % \
                   (dbconfig['user'], dbconfig['pwd'], dbconfig['host'], dbconfig['database'])
     # 连接数据库
     engine = create_engine(connect_str, encoding='utf8')
     try:
+        print('------------开始向%s表插入数据------------' % name)
         readData = read_excel()
-        readData.to_sql(table_name, con=engine, index=False, if_exists='append')
-        print('插入数据成功！')
+        readData.to_sql(name, con=engine, index=False, if_exists='append')
+        print('------------------插入数据成功！------------------')
     except Exception as e:
         print(e)
 
 
-
+if __name__ == '__main__':
+    print('请输入要插入数据的表名：')
+    table_name = str(input())
+    check_table(table_name)
